74264c34dfcd9e7a63152dc5a001651e15faf7cd
mentab <25363363+mentab@users.noreply.github.com>
2018-10-25 20:09:28 +0200

    Updates

---
+++ precache-manifest.272423636517afee1529c3dae2fa1e7f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8507572375d5b5ea7f9",
+    "url": "/random-quote-machine/static/js/runtime~main.b8507572.js"
+  },
+  {
+    "revision": "5c554192f4a87bd8d187",
+    "url": "/random-quote-machine/static/js/main.5c554192.chunk.js"
+  },
+  {
+    "revision": "010c010fc6a473ef82e9",
+    "url": "/random-quote-machine/static/js/1.010c010f.chunk.js"
+  },
+  {
+    "revision": "010c010fc6a473ef82e9",
+    "url": "/random-quote-machine/static/css/1.e2f10f93.chunk.css"
+  },
+  {
+    "revision": "46fbde441b1bbd5f3b9ce55bdc87bbda",
+    "url": "/random-quote-machine/index.html"
+  }
+];
